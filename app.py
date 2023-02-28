f3369b3a4a15f6de830e33fd92b14215999f2632
78redok <81646320+78redok@users.noreply.github.com>
2023-02-27 16:44:35 -0800

    mainmenu v1

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+from interface import mainmenu as mm
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    mm.main_menu();
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
