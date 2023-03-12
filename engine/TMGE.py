72843e391cfde607442abd18164cf9e44bcffab5
ayako54 <54380330+ayako54@users.noreply.github.com>
2023-03-12 14:46:05 +0900

    Revert "minor resolving conflicts"
    This reverts commit a623ee4c299a2484466add37c3dca23969a0dfaa.

--- engine/TMGE.py
+++ engine/TMGE.py
@@ -1,5 +1,4 @@
 import datetime
-from engine.Board.tiles_board import TilesBoard
 
 # from Board.tiles_board import TilesBoard
 # >>> b = datetime.datetime.now()
@@ -8,8 +7,11 @@ from engine.Board.tiles_board import TilesBoard
 # 0:00:05.077263
 # >>> int(delta.total_seconds() * 1000) # milliseconds
 # 5077
+<<<<<<< HEAD
+from engine.Board.tiles_board import TilesBoard
 
-
+=======
+>>>>>>> c081d3274b5d8dfa7b0a725a3c44ff2f5f121087
 
 class TMGE:
     def __init__(self, FPS, GUI):
