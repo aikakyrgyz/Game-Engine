dad64f0ee47356af7059874c4342eb4693a6ccc0
aikakyrgyz <aikamusiconly@gmail.com>
2023-03-18 19:43:30 -0700

    horizontal-vertical matching-still needs work

--- drmario/dmgameplay.py
+++ drmario/dmgameplay.py
@@ -17,7 +17,15 @@ class DrMarioGame(TMGE):
 
     def __init__(self, FPS, GUI):
         TMGE.__init__(self, FPS, GUI)
-        self.tile_types = [["E", "E", "E"],["E", "E", "E"],["E", "E", "E"], ["E", "R", "E"], ["Y", "Y", "Y"]]
+        # self.tile_types = [["E", "E", "E"],["E", "E", "E"],["E", "E", "E"], ["E", "R", "E"], ["Y", "Y", "Y"]]
+        # self.tile_types = [["E", "E", "E"],["E", "E", "Y"],["E", "E", "Y"], ["E", "Y", "R"], ["Y", "Y", "Y"]]
+        # self.tile_types = [["E", "E", "E"],["E", "E", "Y"], ["E", "E", "Y"], ["E", "E", "Y"], ["E", "E", "R"],["E", "E", "Y"], ["E", "R", "Y"], ["Y", "R", "Y"]] #vertical matching
+        # self.tile_types = [["E", "E", "E"],["E", "E", "Y"], ["E", "E", "Y"], ["E", "E", "Y"], ["E", "E", "Y"],["E", "E", "Y"], ["E", "R", "Y"], ["Y", "R", "R"]] #vertical matching case 1
+        self.tile_types = [["E", "E", "E"],["E", "E", "E"], ["E", "E", "E"], ["E", "E", "E"], ["E", "E", "E"],["E", "E", "E"], ["E", "R", "E"], ["Y", "R", "E"]] #vertical matching case 1
+
+        # self.tile_types = [["E", "E", "E", "E"],["E", "E", "E", "Y"],["E", "E", "E", "Y"], ["E", "E", "R", "R"], ["Y", "Y", "R", "Y"]] #horizontal matching test
+        # self.tile_types = [["E", "E", "E", "E"], ["E", "E", "E", "E"],["E", "E", "E", "Y"],["E", "E", "E", "Y"], ["E", "E", "Y", "Y"], ["Y", "Y", "Y", "Y"]] #horizontal matching test
+
         self.tile_size = 20
         # the reason why we want them to be produced from the same factories is for matching purposes.
         # ex: in DrMario redPill and redVirus should be matchable
@@ -36,15 +44,15 @@ class DrMarioGame(TMGE):
         my_tile_board = MyTileBoard(self.tile_size,
                                         self.factory, self.falling_factory,
                                         self.tile_types, self.falling_tile_types,
-                                        2, 2)
+                                        2, 2, 3)
         self.set_tile_board(my_tile_board)
 
     def set_up_my_tile_factories(self):
         # create the main factory
         factory = TileAbstractFactory()
         # initialize each inidividual factory
-        yellow_factory = TileFactory(Yellow, Sprite("yellow-virus.png"), "Y")
-        red_factory = TileFactory(Red, Sprite("red-virus.png"), "R")
+        yellow_factory = TileFactory(Yellow, Sprite("yellow-virus.png"), "Y", False)
+        red_factory = TileFactory(Red, Sprite("red-virus.png"), "R", False)
         # register individual factories within the main factory
         factory.register_factory("Y", yellow_factory)
         factory.register_factory("R", red_factory)
@@ -54,8 +62,8 @@ class DrMarioGame(TMGE):
         falling_factory = TileAbstractFactory()
         # we can take advantage of the sprite, it is still yellow tile
         # but this time it is yellow pill not the virus and since these are different factories there is not conflict between the two
-        yellow_factory = TileFactory(Yellow, Sprite("yellow-pill.png"), "A")
-        red_factory = TileFactory(Red, Sprite("red-pill.png"), "B")
+        yellow_factory = TileFactory(Yellow, Sprite("yellow-pill.png"), "A", False)
+        red_factory = TileFactory(Red, Sprite("red-pill.png"), "B", False)
         falling_factory.register_factory("Y", yellow_factory)
         falling_factory.register_factory("R", red_factory)
         self.set_falling_factory(falling_factory)
--- drmario/myTileBoard.py
+++ drmario/myTileBoard.py
@@ -3,8 +3,8 @@ from engine.Tile.tileAbstractFactory import TileAbstractFactory
 
 
 class MyTileBoard(TilesBoard):
-    def __init__(self, tile_size, tile_factories: TileAbstractFactory, falling_tile_factories: TileAbstractFactory, tile_types, falling_tile_types, min_num_tiles, max_num_tiles):
-        TilesBoard.__init__(self, tile_size, tile_factories, falling_tile_factories, tile_types, falling_tile_types, min_num_tiles, max_num_tiles)
+    def __init__(self, tile_size, tile_factories: TileAbstractFactory, falling_tile_factories: TileAbstractFactory, tile_types, falling_tile_types, min_num_tiles, max_num_tiles, min_match_num):
+        TilesBoard.__init__(self, tile_size, tile_factories, falling_tile_factories, tile_types, falling_tile_types, min_num_tiles, max_num_tiles, min_match_num)
 
     def overriden(f): # for documentation purposes
         return f
@@ -12,6 +12,6 @@ class MyTileBoard(TilesBoard):
     @overriden
     def match_all(self):
         # for DR mario we want both matches
-        self.horizontal_match()
+        # self.horizontal_match()
         self.vertical_match()
 
