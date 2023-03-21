0a2a466d2ecb8088af4f9b0b27980d721f037dd1
doesittaco <114964896+doesittaco@users.noreply.github.com>
2023-03-21 12:56:47 -0700

    fixed dynamic button & image

--- interface/apptheme.py
+++ interface/apptheme.py
@@ -1,13 +1,15 @@
+import os
+
 import pygame_menu
 
 APP_TITLE = "<Placeholder Title>"
 SURFACE_WIDTH = 1000
 SURFACE_HEIGHT = 700
-BACKGROUND_COLOR = (24, 24, 64, 90)    # DARK BLUE
+BACKGROUND_COLOR = (24, 24, 64, 200)  # DARK BLUE
 ACCENT_COLOR = (255, 255, 255)
-SELECT_FONT_COLOR = (170, 226, 255)     # LIGHT BLUE
+SELECT_FONT_COLOR = (170, 226, 255)  # LIGHT BLUE
 TITLE_BACKGR_COLOR = (55, 65, 90, 190)  # GREY BLUE
-HEADING_FONT_COLOR = (250, 231, 100)    # YELLOW
+HEADING_FONT_COLOR = (250, 231, 100)  # YELLOW
 HEADING_FONT = pygame_menu.font.FONT_DIGITAL
 FONT = pygame_menu.font.FONT_MUNRO
 
@@ -19,7 +21,7 @@ def get_theme():
                              title_background_color=TITLE_BACKGR_COLOR,
                              widget_font=FONT,
                              widget_font_color=ACCENT_COLOR,
-                            #  background_color=(0, 0, 0, 0),  # transparent background
+                             # background_color=(0, 0, 0, 0),  # transparent background
                              background_color=BACKGROUND_COLOR,  # 75% opacity
                              # title_bar_style=pygame_menu.widgets.MENUBAR_STYLE_TITLE_ONLY_DIAGONAL,
                              widget_padding=25,
